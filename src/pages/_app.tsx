111d52ecc494f871cdcbe3f22c3f6d4af0f8d26f
EduardooPV <eduardoveltroni@hotmail.com>
2022-06-13 22:00:43 -0300

    feat: adiciona tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,15 @@ import type { AppProps } from "next/app";
 import GlobalStyled from "../styles/GlobalStyled";
 import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-MKB6LXL" });
+  }, []);
+
   return (
     <>
       <GlobalStyled />
